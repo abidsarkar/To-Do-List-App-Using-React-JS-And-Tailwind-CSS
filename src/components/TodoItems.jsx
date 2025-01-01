3bef2a1d19abba0d1364317ddcfcac1098dda1e3
abidsarkar <abidsarkar0@gmail.com>
2025-01-01 14:18:40 +0600

    lignthrough added

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -7,7 +7,7 @@ const TodoItems = ({text, id, isComplete, delateTodo, toggle}) => {
     <div onClick={()=>{toggle(id)}} className='flex items-center my-3 gap-2'>
         <div className='flex flex-1 items-center cursor-pointer'>
             <img className='w-7' src={isComplete ?tick :not_tick} alt="" />
-            <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
+            <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-500 ${isComplete ? "line-through" : ""}`}>{text}</p>
         </div> 
         <img onClick={()=>{delateTodo(id)}} className='w-3.5 cursor-pointer' src={delete_icon} alt="" />
     </div>

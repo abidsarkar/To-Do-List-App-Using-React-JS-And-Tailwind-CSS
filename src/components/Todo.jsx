cbb92116774f3b347717aa75a6bf54ed60fb42f5
abidsarkar <abidsarkar0@gmail.com>
2025-01-01 14:10:04 +0600

    alter todo  icon added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -36,7 +36,7 @@ const Todo = () => {
   };
 
   useEffect(()=>{
-    console.log(todoList);
+    // console.log(todoList);
   },[todoList])
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -6,7 +6,7 @@ const TodoItems = ({text, id, isComplete, delateTodo, toggle}) => {
   return (
     <div onClick={()=>{toggle(id)}} className='flex items-center my-3 gap-2'>
         <div className='flex flex-1 items-center cursor-pointer'>
-            <img className='w-7' src={tick} alt="" />
+            <img className='w-7' src={isComplete ?tick :not_tick} alt="" />
             <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
         </div> 
         <img onClick={()=>{delateTodo(id)}} className='w-3.5 cursor-pointer' src={delete_icon} alt="" />

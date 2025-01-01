c5d392b152dc69cc95e8b0aaafddd4c05a537e51
abidsarkar <abidsarkar0@gmail.com>
2025-01-01 13:47:02 +0600

    delate work

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,23 +2,28 @@ import React, { useRef, useState } from "react";
 import todo_icon from "../assets/todo_icon.png";
 import TodoItems from "./TodoItems";
 const Todo = () => {
-    const[todoList, setTodoList] = useState([]);
-    const inputRef = useRef();
-    const add = ()=>{
-        const inputText = inputRef.current.value.trim();
-        if (inputText==='') {
-            return null;
-        }
-
-        // console.log(inputText);
-        const newTodo = {
-            id: Date.now(),
-            text: inputText,
-            isComplete: false,
-        }
-        setTodoList((prev)=> [...prev, newTodo]);
-        inputRef.current.value ="";
+  const [todoList, setTodoList] = useState([]);
+  const inputRef = useRef();
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+    if (inputText === "") {
+      return null;
     }
+
+    // console.log(inputText);
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
+  const delateTodo = (id) => {
+    setTodoList((prvTodos) => {
+      return prvTodos.filter((todo) => todo.id !== id);
+    });
+  };
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
       {/* -----title ---- */}
@@ -31,19 +36,31 @@ const Todo = () => {
       </div>
       {/* -----input Box ---- */}
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
-        <input ref={inputRef}
+        <input
+          ref={inputRef}
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
           type="text"
           placeholder="Add your task"
         />
-        <button onClick={add} className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">
+        <button
+          onClick={add}
+          className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+        >
           Add +
         </button>
       </div>
       {/* -----todo list ---- */}
       <div>
-        {todoList.map((item,index)=>{
-            return <TodoItems key={index} text={item.text}/>
+        {todoList.map((item, index) => {
+          return (
+            <TodoItems
+              key={index}
+              text={item.text}
+              id={item.id}
+              isComplete={item.isComplete}
+              delateTodo={delateTodo}
+            />
+          );
         })}
         {/* <TodoItems text={"learn coding"}/>
         <TodoItems text={"learn coding from abid"}/> */}

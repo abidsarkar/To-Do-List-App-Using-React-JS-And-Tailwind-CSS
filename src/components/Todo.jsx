ac4b39979cc18f65f44cb5e894a5c73280e1ac46
abidsarkar <abidsarkar0@gmail.com>
2025-01-01 13:21:24 +0600

    useref and button onclick to read input

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useRef } from "react";
 import todo_icon from "../assets/todo_icon.png";
 import TodoItems from "./TodoItems";
 const Todo = () => {
+    const inputRef = useRef();
+    const add = ()=>{
+        const inputText = inputRef.current.value;
+        console.log(inputText);
+    }
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
       {/* -----title ---- */}
@@ -14,19 +19,19 @@ const Todo = () => {
       </div>
       {/* -----input Box ---- */}
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
-        <input
+        <input ref={inputRef}
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
           type="text"
           placeholder="Add your task"
         />
-        <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">
+        <button onClick={add} className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">
           Add +
         </button>
       </div>
       {/* -----todo list ---- */}
       <div>
-        <TodoItems/>
-        <TodoItems/>
+        <TodoItems text={"learn coding"}/>
+        <TodoItems text={"learn coding from abid"}/>
       </div>
     </div>
   );
